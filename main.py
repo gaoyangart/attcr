f00a1211b1fe0f41a8a18969ac82b36f3de176c8
--get-all <gaoyang.art@163.com>
2024-11-06 00:10:16 +0800

    :tada: feat(init): first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+"""
+* @Author       : Gainovel
+* @Organization : Copyright © 2024 gainovel.com All Rights Reserved.
+* @Date         : 2024-11-05 22:47:07 星期二
+* @ProductName  : PyCharm
+* @ProjectName  : attcr
+* @File         : /main.py
+* @Description  : 开发中···
+"""
+
+a = 1
+b = 2
+c = 3
+d = 4
+print("hello")
+print("hello")
+print("hello")
+print("hello")
+print("hello")
